df98d5f622a908a97810be1c51855e7663367503
javadev <javadev75@gmail.com>
2013-07-06 05:47:05 +0300

    Added index.html support

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var data = fs.readFileSync('index.html');
+  response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
